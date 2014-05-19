6a6f568f9b09fc192a239131a620b8b56c625c06
Javier Cabezas <jcleyton@gmail.com>
2014-05-19 17:07:29 -0400

    Added YiiBooster 3.0.1

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,74 +1,81 @@
 <?php
+Yii::setPathOfAlias('bootstrap', dirname(__FILE__) . '/../extensions/bootstrap');
 
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
-
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Daisuki Pokéapp',
-
-	'preload'=>array('log'),
-
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
-
-	'modules'=>array(
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'gii',
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-	),
-
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=pokeapp',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		'errorHandler'=>array(
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-	),
-
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'jcleyton@gmail.com',
-	),
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Daisuki Pokéapp',
+    
+    'preload' => array(
+        'log',
+        'bootstrap',
+    ),
+    
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*'
+    ),
+    
+    'modules' => array(
+        'gii' => array(
+            'class' => 'system.gii.GiiModule',
+            'password' => 'gii',
+            'ipFilters' => array(
+                '127.0.0.1',
+                '::1'
+            ),
+            'generatorPaths' => array(
+            	'bootstrap.gii'
+        	),
+        ),
+    ),
+    
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true
+        ),
+        'bootstrap' => array(
+            'class' => 'bootstrap.components.Bootstrap'
+        ),
+        // uncomment the following to enable URLs in path-format
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>'
+            )
+        ),
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=pokeapp',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8'
+        ),
+        'errorHandler' => array(
+            'errorAction' => 'site/error'
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning'
+                )
+                // uncomment the following to show log messages on web pages
+                /*
+                array(
+                'class'=>'CWebLogRoute',
+                ),
+                */
+            )
+        )
+    ),
+    
+    'params' => array(
+        'adminEmail' => 'jcleyton@gmail.com'
+    )
 );
