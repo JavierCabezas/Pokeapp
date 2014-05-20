654f22b04f6dc508af9ca3fe9ca068559be1f426
Javier Cabezas <jcleyton@gmail.com>
2014-05-20 15:17:30 -0400

    Main pokéball module form is done.

--- protected/modules/pokeball/controllers/PokeballController.php
+++ protected/modules/pokeball/controllers/PokeballController.php
@@ -6,15 +6,14 @@ class PokeballController extends Controller
     {
     	$array_pokeymans = CHtml::listData(PokemonSpecies::model()->findAll(), 'id', 'nombrePokemon'); 
         $array_pokeballs = CHtml::listData(Pokeball::model()->findAll(), 'id', 'nombrePokeball'); 
+		$array_turnos 	 = array('1','2','3','4','5','6','7','8','9', '10', '11', '12', '13', '14', '15', '16', '17', 
+								'18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '30+');
 
-        for ($i = 1; $i < 30; $i++)
-            $array_turnos[$i] = $i;
-        $array_turnos['30'] = "30+";
-        
+		array_unshift($array_pokeymans, 'Elige un pokémon');
         $this->render('index', array(
             'array_pokeymans' => $array_pokeymans,
             'array_pokeballs' => $array_pokeballs,
-            'array_turnos' => $array_turnos
+            'array_turnos' => $array_turnos,
         ));
     }
 }
--- protected/modules/pokeball/views/pokeball/index.php
+++ protected/modules/pokeball/views/pokeball/index.php
@@ -10,7 +10,7 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 
 	<table border="1">
 		<tr><!-- Pokémon -->
-			<td> Pokémon a capturar </td>
+			<td> <h4> Pokémon a capturar </h4> </td>
 			<td>
 				<?php
 					$this->widget(
@@ -28,7 +28,7 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 			<td> <div id="pokemon_elegido"> <?php echo CHtml::image(Yii::app()->baseUrl.'/images/sprites/0.png'); ?>  </div></td>
 		</tr>
 		<tr><!-- Pokéball -->
-			<td> Pokéball a usar </td>
+			<td> <h4> Pokéball a utilizar </h4> </td>
 			<td>
 				<?php
 					$this->widget(
@@ -46,18 +46,18 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 			<td> <div id="foto_pokeball" style="height:100px"> <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pokeball/pball1.png'); ?> </div> </td>
 		</tr>
 		<tr> <!-- Generación -->
-			<td> Generación </td>
+			<td> <h4> Generación </h4> </td>
 			<td> <select id="gen" name="gen">
-	   				<option value="2">Second (GS-C)</option>
-	     			<option value="3">Third (RS-E-FRLG)</option>
-	  				<option value="4">Fourth (DP-Pl-HGSS)</option>
-	   				<option value="5">Fifth (BW-BW2)</option>
+	   				<option value="2">Segunda (GS-C)</option>
+	     			<option value="3">Tercera (RS-E-FRLG)</option>
+	  				<option value="4">Cuarta (DP-Pl-HGSS)</option>
+	   				<option value="5">Quinta (BW-BW2)</option>
 				</select>
 			</td> 
 			<td> <div id="foto_gen" class="pball_container" style="height:100px"> <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pokeball/gen2.png'); ?> </div> </td> 
 		</tr>
 		<tr> <!-- Status aligment -->
-			<td> Status ailment </td>
+			<td> <h4> Status ailment </h4> </td>
 			<td>
 				<select id="status" name="status">
 	  				<option value="Normal">Normal</option>
@@ -71,119 +71,231 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 			<td> <div id="foto_status" class="pball_container" style="height:70px"> <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pokeball/statusNormal.png'); ?> </td>
 		</tr>
 		<tr>
-			<td> Porcentaje HP (número entero entre 1 y 100) </td>
+			<td> <u class ="dotted"> <span title="Número entero entre 1 y 100 "> <h4>Porcentaje HP </h4> </span> </td>
 			<td>
 				<input type="number" name="hp_percentage" id='hp_percentage' value='100' min="1" max="100">
 			</td>
 			<td> <div id="hp_percentage_photo"> <?php echo CHtml::image(Yii::app()->baseUrl.'/images/pokeball/hp10.png'); ?>   </div> </td>
 		</tr>
-
 	</table>
 
 	<div class="clear"> &nbsp; </div>
 
 	<!-- Área divs por pokéball -->
-	<div class='pball_especial' id="dive_ball">
-		<p>	¿El encuentro ocurrió pescando/surfeando/bajo el agua? </p>
-		<select id="select_diveball" name="select_diveball">
-	  		<option value="1"> Sí </option>
-	   		<option value="0"> No </option>
-		</select>
-	</div>
+	<table class='pball_especial' id="dive_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factores exclusivo de Dive Ball (Buceo Ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿El encuentro ocurrió pescando/surfeando/bajo el agua? 
+			</td>
+			<td>
+				<select id="select_diveball" name="select_diveball">
+			  		<option value="1"> Sí </option>
+			   		<option value="0"> No </option>
+				</select>
+			</td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="nest_ball">
-		<p> <input type="number" name="select_nestball" id='select_nestball' value='100' min="1" max="100"> </p>
-	</div>
+	<table class='pball_especial' id="nest_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor exclusivo de Nest Ball (Nido Ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿Cuál es el nivel del pokémon que quieres capturar? 
+			</td>
+			<td>
+				<input type="number" name="select_nestball" id='select_nestball' value='100' min="1" max="100">
+			</td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="repeat_ball">
-		<p>	¿El pokémon fue capturado previamente? </p>
-		<select id="select_repeatball" name="select_repeatball">
-	  			<option value="1"> Sí </option>
-	   			<option value="0"> No </option>
-			</select>
-	</div>
+	<table class='pball_especial' id="repeat_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor exclusivo de Repeat Ball <u class ="dotted"><span title="Acopio ball?! ¿Por qué hay jugadores que siguen en español?">(Acopio Ball) </span> </u> </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿El pokémon fue capturado previamente?
+			</td>
+			<td>
+				<select id="select_repeatball" name="select_repeatball">
+		  			<option value="1"> Sí </option>
+		   			<option value="0"> No </option>
+				</select>
+			</td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="timer_ball">
-		<p> ¿Cuántos turnos han transcurrido en la pelea? </p>
-			<?php echo CHtml::dropDownList('select_timerball', '',  $array_turnos); ?> 
-	</div>
+	<table class='pball_especial' id="timer_ball">
+		<tr>
+			<td colspan='3'> 
+				<h3> Factor exclusivo de Timer Ball (Turno ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿Cuántos turnos han transcurrido en la pelea? 
+			</td>
+			<td>
+				<?php echo CHtml::dropDownList('select_timerball', '',  $array_turnos); ?> 
+			</td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="dusk_ball">
-			<p> ¿Se el pokémon salvaje está en una cueva o es de noche? </p>
-			<select id="select_duskball" name="select_duskball">
-	  			<option value="1"> Sí </option>
-	   			<option value="0"> No </option>
-			</select>
-	</div>
+	<table  class='pball_especial' id="dusk_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor exclusivo Dusk Ball (Ocaso Ball) </h3>
+			</td> 
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿Se el pokémon salvaje está en una cueva o es de noche?
+			</td>
+			<td>
+				<select id="select_duskball" name="select_duskball">
+		  			<option value="1"> Sí </option>
+		   			<option value="0"> No </option>
+				</select>
+			</td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="quick_ball">
-		<p> ¿Se usó la Quick Ball en el primer turno de la batalla? </p>
-		<p>	<select id="select_quickball" name="select_quickball">
-	  			<option value="1"> Sí </option>
-	   			<option value="0"> No </option>
-			</select> 
-		</p>
-	</div>
+	<table class='pball_especial' id="quick_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor único de Quick Ball (Veloz Ball) </h3>
+			</td>
+		</tr>
+		<tr> 
+			<td colspan='2'>
+				¿Se usó la Quick Ball en el primer turno de la batalla? 
+			</td>
+			<td>
+				<select id="select_quickball" name="select_quickball">
+		  			<option value="1"> Sí </option>
+		   			<option value="0"> No </option>
+				</select>
+			</td> 
+		</tr>
+	</table>
 
-	<div class='pball_especial'  id="level_ball">
-		<p> <input type="number" name="select_levelball_nivel_oponente" id='select_levelball_nivel_oponente' value='100' min="1" max="100"> </p>
-		<p> <input type="number" name="select_levelball_nivel_jugador" id='hp_percentage' value='100' min="1" max="100"> </p>
-	</div>
+	<table class='pball_especial'  id="level_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factores únicos de Level Ball (Nivel Ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				Nivel del oponente
+			</td>
+			<td>
+				<input type="number" name="select_levelball_nivel_oponente" id='select_levelball_nivel_oponente' value='100' min="1" max="100"> 
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				Nivel de tu pokémon activo
+			</td>
+			<td>
+				<input type="number" name="select_levelball_nivel_jugador" id='hp_percentage' value='100' min="1" max="100">
+			</td>
+		</tr>		
+	</table>
 
-	<div class='pball_especial' id="lure_ball">
-		<p> ¿El pokémon a capturar fue encontrado por medio de pesca?"
-			<select id="select_lureball" name="select_lureball">
-				<option value="1"> Sí </option>
-	   			<option value="0"> No </option>
-		 	</select>
-		</p>
-	</div>
+	<table class='pball_especial' id="lure_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor único por Lure Ball (Cebo Ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'> 
+				¿El pokémon a capturar fue encontrado pescando?
+			</td>
+			<td>
+				<select id="select_lureball" name="select_lureball">
+					<option value="1"> Sí </option>
+		   			<option value="0"> No </option>
+			 	</select>
+			 </td>
+		</tr>
+	</table>
 
-	<div class='pball_especial' id="love_ball">
-		<p> ¿Es el pokémon oponente de la misma especie (es decir, el MISMO pokémon) y género contrario? </p>
-		<p>	<select id="select_loveball" name="select_loveball">
-	  			<option value="1"> Sí </option>
-	   			<option value="0"> No </option>
-			</select>
-		</p>
-	</div>
+	<table class='pball_especial' id="love_ball">
+		<tr>
+			<td colspan='3'>
+				<h3> Factor único de Love Ball (Amor Ball) </h3>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				¿Es el pokémon oponente de la misma especie (es decir, el MISMO pokémon) y género contrario? 
+			</td>
+			<td>	
+				<select id="select_loveball" name="select_loveball">
+		  			<option value="1"> Sí </option>
+		   			<option value="0"> No </option>
+				</select>
+			</td>
+		</tr>
+	</table>
 	<!-- Fin divs por pokéball -->
 
 	<!-- Área Quinta generación -->
-	<div class='fifthgen'  id="quinta_grass">
-		<p> ¿Estás en hierva alta (high grass, donde hay probabilidad de encuentros con dos pokémon al unísono)? </p>
-		<p>	<select id="select_grass" name="select_grass">
-	  			<option value="0"> No </option>
-	  			<option value="1"> Sí </option>
-			</select>
-		</p>
-	</div>
-
-	<div class='fifthgen' id="quinta_numpokemon">
-		<p> ¿Cuántos pokémon tienes como <b>capturados</b> en la pokédex? (Si, es un factor =P)</p>
-		<p>	<select id="select_numpokemon" name="select_numpokemon">
-	  			<option value="3"> 1-30 </option>
-	   			<option value="5"> 31-150 </option>
-	   			<option value="7"> 151-300 </option>
-	   			<option value="8"> 301-450 </option>
-	   			<option value="9"> 451-600 </option>
-	   			<option value="10">600-649 </option>
-			</select>
-		</p>
-	</div>
-
-	<div class='fifthgen' id="quinta_entralink">
-		<p> ¿Está activado algún poder de entralink? </p>
-		<p>	<select id="select_entralink" name="select_entralink">
-	  			<option value="10"> Ninguno </option>
-	  			<option value="11"> Power ↑ </option>
-	  			<option value="12"> Power ↑↑ </option>
-	  			<option value="13"> Power ↑↑↑ </option>
-	  			<option value="13"> Power S </option>
-	  			<option value="13"> Power MAX </option>
-			</select>
-		</p>
-	</div>
+	<h3 class='fifthgen'> Factores exclusivos de quinta generación </h3>
+	<table class='fifthgen'>
+		<tr>
+			<td colspan='2'>
+				<u class ="dotted"><span title="La hierva alta es donde hay probabilidad de tener encuentro con dos pokémon al unísono."> ¿Estás en hierva alta (high grass)? </span> </u>
+			</td>
+			<td>
+				<select id="select_grass" name="select_grass">
+					 		<option value="0"> No </option>
+					 		<option value="1"> Sí </option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'>
+				<u class ="dotted"><span title="Si, les prometo que esto es un factor =P"> ¿Cuántos pokémon tienes como <b>capturados</b> según tu pokédex? </span> </u>
+			</td>
+			<td>
+				<select id="select_numpokemon" name="select_numpokemon">
+		  			<option value="3"> 1-30 </option>
+		   			<option value="5"> 31-150 </option>
+		   			<option value="7"> 151-300 </option>
+		   			<option value="8"> 301-450 </option>
+		   			<option value="9"> 451-600 </option>
+		   			<option value="10">600-649 </option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='2'> ¿Está activado algún poder de entralink? </td>
+			<td> 
+				<select id="select_entralink" name="select_entralink">
+		  			<option value="10"> Ninguno </option>
+		  			<option value="11"> Power ↑ </option>
+		  			<option value="12"> Power ↑↑ </option>
+		  			<option value="13"> Power ↑↑↑ </option>
+		  			<option value="13"> Power S </option>
+		  			<option value="13"> Power MAX </option>
+				</select>
+			</td>
+		</tr>
+	</table>
 	<!-- Fin Quinta generación -->
 
 	 <?php echo CHtml::ajaxButton("Calcular probabilidad",
@@ -205,40 +317,26 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 
 		//Preload images
 		var domain = '<?php echo Yii::app()->request->baseUrl ?>';
-		preloadImg( domain + '/images/pokeball/gen1.png');
-		preloadImg( domain + '/images/pokeball/gen2.png');
-		preloadImg( domain + '/images/pokeball/gen3.png');
-		preloadImg( domain + '/images/pokeball/gen4.png');
-		preloadImg( domain + '/images/pokeball/gen5.png');
-		preloadImg( domain + '/images/pokeball/hp0.png');
-		preloadImg( domain + '/images/pokeball/hp1.png');
-		preloadImg( domain + '/images/pokeball/hp2.png');
-		preloadImg( domain + '/images/pokeball/hp3.png');
-		preloadImg( domain + '/images/pokeball/hp4.png');
-		preloadImg( domain + '/images/pokeball/hp5.png');
-		preloadImg( domain + '/images/pokeball/hp6.png');
-		preloadImg( domain + '/images/pokeball/hp7.png');
-		preloadImg( domain + '/images/pokeball/hp8.png');
-		preloadImg( domain + '/images/pokeball/hp9.png');
-		preloadImg( domain + '/images/pokeball/hp10.png');
-		
+
 		//Generation functions
 	    $('#gen').change(function() {
     		var genno = parseInt($('#gen').val());
     		$('#foto_gen').html("<img src='/pokeapp/images/pokeball/gen"+genno+".png'>");
 
-    		if(gen==5)
+    		if(genno==5)
     			$('.fifthgen').show();
     		else
     			$('.fifthgen').hide();
-
     	});
 
     	//Pokéball functions
-		$('#pokeball_a_usar').change(function() {
+		$('#pokeball_to_use').change(function() {
+
+			var pokeball = parseInt($('#pokeball_to_use').val());
+			preloadImg( domain + '/images/pokeball/pball'+pokeball+'.png');
+
 			$('.pball_especial').hide();
-			var id_pokeball = parseInt($('#pokeball_a_usar').val());
-			switch(id_pokeball) {
+			switch(pokeball) {
 				case 7:	 $('#dive_ball').show();	break;
 				case 8:  $('#nest_ball').show();	break;
 				case 9:  $('#repeat_ball').show();	break;
@@ -249,13 +347,28 @@ En vez de limitar la calculadora para no poder hacer casos imposibles (Ej: Captu
 				case 19: $('#lure_ball').show();	break;
 				case 21: $('#love_ball').show();	break;
 			}
+			$('#foto_pokeball').html("<img src='/pokeapp/images/pokeball/pball"+pokeball+".png'>");
     	});
 
 		//Update the HP bar.
     	$('#hp_percentage').change(function() {
     		var hp =  parseInt((parseInt($('#hp_percentage').val()))/10);
-    		$('#hp_percentage_photo').html("<img src='/pokeapp/images/pokeball/hp"+hp+".png'>");
+    		preloadImg( domain + '/images/pokeball/hp'+hp+'.png');
+    		setTimeout(function(){ $('#hp_percentage_photo').html("<img src='/pokeapp/images/pokeball/hp"+hp+".png'>") } , 400);
     	});
+
+    	//Update status
+    	$('#status').change(function() {
+    		var status = $('#status').val();
+			preloadImg( domain + '/images/pokeball/status'+status+'.png');
+			setTimeout(function(){ $('#foto_status').html("<img src='/pokeapp/images/pokeball/status"+status+".png'>") }, 800);
+    	});
+
+		$('#pokemon_to_capture').change(function() {
+			var pokemon_id = $('#pokemon_to_capture').val();
+			preloadImg( domain + '/images/sprites/'+pokemon_id+'.png');
+			setTimeout(function(){ $('#pokemon_elegido').html("<img src='/pokeapp/images/sprites/"+pokemon_id+".png'>") }, 800);
+		});
 
 	});
 </script>
