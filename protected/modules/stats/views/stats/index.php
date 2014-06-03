3eae4a9d31b173c47c906eeb563a74ee2a4d959b
Fernanda <fernanda.kauak@gmail.com>
2014-06-03 00:19:54 -0400

    New CSS file and organization of the stats data.

--- protected/modules/stats/views/stats/index.php
+++ protected/modules/stats/views/stats/index.php
@@ -4,36 +4,41 @@
 los stats de tus pokémon antes de entrenarlos o estimar cuantos EV necesitarás entregarle a tu pokémon. </p>
 
 <!-- Render the two forms partials -->
-<div class='pokemon_1'>
-	<?php $this->renderPartial('_form', array(
-		'n' 			  		=> 1,
-		'array_pokeymans' 		=> $array_pokeymans,
-		'array_items' 	  		=> $array_items,
-		'array_nature'			=> $array_nature,
-		));
-	?>
-</div> <!-- end of form 1 -->
 
-<div clas='pokemon_2'>
-	<?php $this->renderPartial('_form', array(
-		'n' 			  		=> 2,
-		'array_pokeymans' 		=> $array_pokeymans,
-		'array_items' 			=> $array_items,
-		'array_nature'			=> $array_nature,
-		));
-	?>
-</div> <!-- end of form 2 -->
+<div id="pokemon_info">
+	<div class='pokemon_1'>
+		<?php $this->renderPartial('_form', array(
+			'n' 			  		=> 1,
+			'array_pokeymans' 		=> $array_pokeymans,
+			'array_items' 	  		=> $array_items,
+			'array_nature'			=> $array_nature,
+			));
+		?>
+	</div> <!-- end of form 1 -->
 
-<div class='result_1'>
-	<?php $this->renderPartial('_showResults', array(
-		'n'		 => 1,
-		'pokemon' => null
-	)); ?>
-</div> <!-- end of results 1 -->
+	<div class='pokemon_2'>
+		<?php $this->renderPartial('_form', array(
+			'n' 			  		=> 2,
+			'array_pokeymans' 		=> $array_pokeymans,
+			'array_items' 			=> $array_items,
+			'array_nature'			=> $array_nature,
+			));
+		?>
+	</div> <!-- end of form 2 -->
+</div>
 
-<div class='result_2'>
-	<?php $this->renderPartial('_showResults', array(
-		'n'		 => 2,
-		'pokemon' => null
-	)); ?>
-</div> <!-- end of results 2 -->
+<div id="results_info">
+	<div class='result_1'>
+		<?php $this->renderPartial('_showResults', array(
+			'n'		 => 1,
+			'pokemon' => null
+		)); ?>
+	</div> <!-- end of results 1 -->
+
+	<div class='result_2'>
+		<?php $this->renderPartial('_showResults', array(
+			'n'		 => 2,
+			'pokemon' => null
+		)); ?>
+	</div> <!-- end of results 2 -->
+</div>
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,8 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/stats.css" />
+	
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
