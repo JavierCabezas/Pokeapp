dbd79ccb05b79d64462ee2f1728d2acfb8deb4b0
Javier Cabezas <jcleyton@gmail.com>
2014-06-03 21:53:57 -0400

    Some info in the "about" section.

--- protected/views/site/about.php
+++ protected/views/site/about.php
@@ -1,13 +1,31 @@
 <h1> Sobre Javier </h1>
 
 <p align="justify"> <?php echo  $foto_yo ?> Hola! Mi nombre es Javier y desde Junio del 2013 que estoy encargado de la sección <b>"Bajo el camión del S.S. Anne"</b> y también soy el autor de la <b>Pokéapp</b> acá en <a href="http://www.pokedaisuki.cl">PokéDaisuki</a>. 
-Si han asistido a un evento de la comunidad posiblemente me hayan visto con mi notebook en la sección del antidopping.
+Si han asistido a un evento de la comunidad posiblemente me hayan visto con mi notebook en la sección del  <u class ="dotted"><span title="La aplicación antidopping fue la primera que hice para Pokédaisuki">antidopping</span></u> del torneo.
 Soy estudiante de sexto año de <u class ="dotted"><span title="Que es como una mezcla de informática con electrónica">Ingeniería Civil Telemática</span></u> en la <u class="dotted"> <span title="Universidad sTécnica Federico Santa María">UTFSM</span></u> y tengo un gran interés en lo que es el área de computadores. A eso agreguémole que me encanta en demasía pokémon, y estoy jugándolo desde la primera generación.
 Es por esto que me encanta entender a fondo, a nivel técnico, el funcionamiento de <u class="dotted"><span title="Lo cual digo para quedar bien en la comunidad nomás, la verdad mi favorito es Tales of Phantasia <3. También disfruto MUCHO Batman Arkham City y toda la saga Ace Attorney.">mi videojuego favorito<span></u>.</p>
 <p align="justify"> Si desean contactarme por cualquier motivo pueden escribirme un correo a <?php echo $foto_mail ?> o agregarme a skype a mi cuenta <b>live:jcleyton</b>. Estoy súper dispuesto a recibir correcciones de cualquier tipo (ya sean de algún planteamiento matemático mal hecho, bugs, ortografía, etc.). Mil gracias de antemano si me escriben por eso! Me puedo equivocar =P. Si tienen alguna consulta
-por alguno de mis artículos también siéntase totalmente libres de escribirme.</p>
-<p align="justify"> Aprovecho de dar las gracias a <a href="http://fermotousan.cl">Fernanda Kauak</a> por su ayuda con respecto a lo que es <u class="dotted"> <span title="Yo soy súper torpe en ese aspecto">diseño de la aplicación</span></u> y a los chicos de Daisuki por darme la oportunidad de hacer mis ñoñerías en la página de una comunidad tan bacán como esta. Los adoro <3.  </p>
+por alguno de mis artículos, tienen ganas de un duelo pokémon o quieren decirme hola siéntase totalmente libres de escribirme!</p>
+<p align="justify"> Aprovecho de dar las gracias a <a href="http://fermotousan.cl">Fernanda Kauak</a> por su ayuda con respecto a lo que es <u class="dotted"> <span title="Yo soy súper torpe en ese aspecto">diseño de la aplicación</span></u> y a los chicos de Daisuki por darme la oportunidad de hacer mis ñoñerías en la página de una comunidad tan bacán como esta. Por mucho que los tenga medios abandonados los adoro &lt; 3.  </p>
 
+<h2> Sobre la Pokéapp </h2>
+<p align="justify"> Dado que además de pokémon gusta harto el asunto de programar decidí de usar mis conocimientos para hacer la cúspide de lo ñoño: 
+Hacer una aplicación de pokémon. Es por esto que me propuse a hacer distintas calculadoras con el fin de facilitarles un poco la vida a los jugadores del 
+mismo. Las hice pensando en que me fuesen útiles a mi como jugador del juego así que espero que lo sean también para ustedes!. 
+</p>
+
+<p align="justify"> 
+Además, para los que estén interesados en programación: decidí lanzar el código fuente de la misma aplicación en un <?php echo CHtml::link('repositorio en GitHub', 'https://github.com/JavierCabezas/Pokeapp') ?> 
+que está libre para cualquiera que lo quiera descargar, ver o editar. La aplicación está hecha con lenguaje de programación php, trabaja con una base de datos MySQL y 
+usa el <u class ="dotted"><span title="Que es un framework MCV fabuloso, lo recomiendo totalmente.">framework Yii</span></u>. Además acá pueden ir viendo las actualizaciones que les vaya heciendo a la aplicación.
+</p>
+
+<p align="justify"> Doy infinitas gracias a los chicos de <?php echo CHtml::link('veekun', 'http://www.veekun.com') ?> que, además de tener un pokédex completísimo, tienen 
+la base de datos que yo usé para este proyecto. Ellos también usan un repositorio en GitHub que pueden ubicar <?php echo CHtml::link('el siguiente link', 'https://github.com/veekun/pokedex') ?>.
+</p>
+
+
+<h2> Artículos que he escrito </h2>
 <p> Los artículos que he escrito para la web son:
 	<ol>
 		<li> <a href="http://www.pokedaisuki.cl/bajo-el-camion-del-s-s-anne-n1-introduccion-a-hexadecimal-y-binario/">Introducción a hexadecimal y binario.</a> </li>
