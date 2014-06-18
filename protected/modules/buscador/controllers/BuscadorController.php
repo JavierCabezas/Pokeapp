f30d2500bfdc2cfcfe7c91374d5439dffaecc6bc
Javier Cabezas <jcleyton@gmail.com>
2014-06-18 12:35:34 -0400

    Updates in the search module.

--- protected/modules/buscador/controllers/BuscadorController.php
+++ protected/modules/buscador/controllers/BuscadorController.php
@@ -4,16 +4,22 @@ class BuscadorController extends Controller
 {
 	public function actionIndex()
 	{
+		$type_criteria = new CDbCriteria;
+        $type_criteria->addCondition("id != 10001"); //Exclude unkown type
+        $type_criteria->addCondition("id != 10002"); //Exlude shadow type
+
 		$array_generations 	= array('1' => 'Primera', '2' => 'Segunda', '3' => 'Tercera', '4' => 'Cuarta', '5' => 'Quinta', '6' => 'Sexta');
 		$array_colors 		= CHtml::listData(PokemonColor::model()->findAll(), 'id', 'colorName');
 		$array_egg_groups 	= CHtml::listData(EggGroups::model()->findAll(), 'id', 'eggGroupName');
 		$array_shapes 		= CHtml::listData(PokemonShapes::model()->findAll(), 'id', 'shapeName');
+		$array_types        = CHtml::listData(Types::model()->findAll($type_criteria), 'id', 'typeName');
 
 		$this->render('index', array(
 			'array_generations' => $array_generations,
 			'array_colors'		=> $array_colors,
 			'array_egg_groups' 	=> $array_egg_groups,
 			'array_shapes'		=> $array_shapes,
+			'array_types'		=> $array_types,
 		));
 	}
 }
--- protected/modules/buscador/views/buscador/index.php
+++ protected/modules/buscador/views/buscador/index.php
@@ -1,84 +1,189 @@
+<!-- START OF SEARCH CRITERIA -->
 <h1> Buscador de pokémon por criterios interesantes </h1>
 
 <div class='search_criteria'>
-<h3> Quiero un pokémon que... </h3>
+	<h3> Quiero un pokémon que... </h3>
+	
 	<div class='height'>
-		<label> Tamaño: </label>
-		
-		Desde:
-		<input type="number" name="height_min" id='height_min' min="1" max="145">
-
-		Hasta:
-		<input type="number" name="height_max" id='height_max' min="1" max="145">
-
-		<div class ='button_height'>  <img src='<?php echo imageDir()?>/nav/arrow.png' alt='agregar' /> </div>
-	</div>
+		<h4> Tamaño: </h4>
+		<label> Desde: </label>
+		<input type="number" class='height_form' name="height_min" id='height_min' min="1" max="145">
+		<label> Hasta: </label>
+		<input type="number" class='height_form' name="height_max" id='height_max' min="1" max="145">
+	</div> 	<!-- end of height -->
 
 	<div class='weight'>
-		<label> Peso (Kilogramos) : </label>
-		
-		Desde:
-		<input type="number" name="weight_min" id='weight_min' min="0.1" max="398" step='0.1'>
+		<h4> Peso (Kilogramos) : </h4>
+		<label> Desde: </label>
+		<input type="number" class='weigth_form' name="weight_min" id='weight_min' min="0.1" max="398" step='0.1'>
+		<label> Hasta: </label>
+		<input type="number" class='weigth_form' name="weight_max" id='weight_max' min="0.1" max="398" step='0.1'>
+	</div> 	<!-- end of weight -->
 
-		Hasta:
-		<input type="number" name="weight_max" id='weight_max' min="0.1" max="398" step='0.1'>
-
-	</div>
-
+	<div class='type'>
+		<h4> Tipo(s) </h4>
+		<label> Tipo 1: </label>
+		<?php echo CHtml::dropDownList('type_1', null, $array_types, array('empty' => '(Ingresar tipo 1)', 'class' => 'type_dropdown'));  ?>
+		<label> Tipo 2: </label>
+		<?php echo CHtml::dropDownList('type_2', null, $array_types, array('empty' => '(Ingresar tipo 2)', 'class' => 'type_dropdown'));  ?>
+	</div>	<!-- end of type -->
 
 	<div class='gen'>
-		<label> Generación: </label>
-		<?php echo CHtml::dropDownList('gen', '', $array_generations, array('empty' => '(Seleccionar generación)')); ?>
-
-	</div>
+		<h4> Generaciones </h4>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_1' value="1"> Primera <br>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_2' value="2"> Segunda <br>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_3' value="3"> Tercera <br>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_4' value="4"> Cuarta <br>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_5' value="5"> Quinta <br>
+			<input type="checkbox" class="gen_checkbox" id='gen_checkbox_6' value="6"> Sexta  <br>
+	</div> 	<!-- end of gen -->
 
 	<div class='color'>
-		<label> Color: </label>
+		<h4> Color: </h4>
 		<?php echo CHtml::dropDownList('color', '', $array_colors, array('empty' => '(Seleccionar color)')); ?>
 	</div>
 
 	<div class='egg'>
-		<label> Grupo huevo: </label>
+		<h4> Grupo huevo: </h4>
 		<?php echo CHtml::dropDownList('egg', '', $array_egg_groups, array('empty' => '(Seleccionar grupo)')); ?>
 	</div>
 
 	<div class='shape'>
-		<label> Forma: </label>
+		<h4> Forma: </h4>
 		<?php echo CHtml::dropDownList('shape', '', $array_shapes, array('empty' => '(Seleccionar forma)')); ?>
 	</div>
-</div> <!-- End of search criteria -->
+</div> 
 
+<!-- END OF SEARCH CRITERIA -->
+
+
+<!-- START OF RESULTS -->
 <div class='search_results'>
 	<h3> Criterios de búsqueda agregados </h3>
+
 	<div class='height_results'>
-		<p visible='hidden'> Desde <span class='height_from'> </span> [kg] hasta <span class='height_to'> </span>
-	</div>
+		<p> Desde <span class='height' id='height_from'> </span> [m] hasta <span class='height' id='height_to'> </span> [m] </p>
+		<div class='height_remove'>  <img src='<?php echo imageDir()?>/buscador/remove.png' alt='sacar' /></div>		
+	</div> <!-- end of height results -->
+
+	<div class='weight_results'>
+		<p> Desde <span class='weight' id='weight_from'> </span> [kg] hasta <span class='weight' id='weight_to'> </span> [kg] </p>
+		<div class='weight_remove'>  <img src='<?php echo imageDir()?>/buscador/remove.png' alt='sacar' /></div>
+	</div> <!-- end of weight results -->
 
 	<div class='gen_results'>
-		<label> Generaciones </label>
-		<div class='result_gen'>
-			<input type="radio" name="radio_gen" value="allow"> Permitir  <br>
-			<input type="radio" name="radio_gen" value="deny" checked> Denegar <br>
-		</div>
+		<p> Se mostrarán pokémon de las generaciones... </p>
+		<ul>
+			<li id='gen_result_1'> Primera </li>
+			<li id='gen_result_2'> Segunda </li>
+			<li id='gen_result_3'> Tercera </li>
+			<li id='gen_result_4'> Cuarta </li>
+			<li id='gen_result_5'> Quinta </li>
+			<li id='gen_result_6'> Sexta </li>
+		</ul>
+	</div>
+
+	<div class='color_results'>
+
 	</div>
 </div>
+<!-- END OF RESULTS -->
 
+<!-- START OF JAVASCRIPT -->
 <script type='text/javascript'>
-	//Height
-	$(".button_height").click(function(){
+	$(".height_results").children().hide();
+	$(".gen_results").children().hide();
 
+	//Height
+	$(".height_form").change(function(){
+		min = parseInt($('#height_min').val());
+		max = parseInt($('#height_max').val());
+
+		if((min + 0 == min)) { //This is what happends when you program without internet. ¿How was the function to see if it was a number called...?
+			$(".height_results").children().show();
+			if(min <= max){	
+				$('#height_from').html(min);
+				$('#height_to').html(max);
+			}else{
+				$('#height_from').html(max);
+				$('#height_to').html(min);
+			}
+		}else{
+			$(".height_results").children().hide();
+		}
 	});
 
+	$(".height_remove").click(function(){
+		$(".height_results").children().hide();
+		$("#height_min").val('');
+		$("#height_max").val('');
+	});
+	//End of Height
+
+	//Weight
+	$(".weigth_form").change(function(){
+		min = parseInt($('#weight_min').val());
+		max = parseInt($('#weight_max').val());
+
+		if((min + 0 == min)) {
+			$(".weight_results").children().show();
+			if(min <= max){	
+				$('#weight_from').html(min);
+				$('#weight_to').html(max);
+			}else{
+				$('#weight_from').html(max);
+				$('#weight_to').html(min);
+			}
+		}else{
+			$(".weight_results").children().hide();
+		}
+	});
+
+	$(".weight_remove").click(function(){
+		$(".weight_results").children().hide();
+		$("#weight_min").val('');
+		$("#weight_max").val('');
+	});
+	//End of Weight
+
+	//Generation checkboxes
+	$('.gen_checkbox').change(function() {
+
+		is_something_ticked =  	($('#gen_checkbox_1')).is(":checked")||($('#gen_checkbox_2')).is(":checked")||($('#gen_checkbox_3')).is(":checked")||
+								($('#gen_checkbox_4')).is(":checked")||($('#gen_checkbox_5')).is(":checked")||($('#gen_checkbox_6')).is(":checked"); 
+
+		if(!is_something_ticked)
+			$(".gen_results").children().hide();
+		else
+			$(".gen_results").children().show();
+
+		for(i = 1 ; i < 7 ; i++){
+			if( ($('#gen_checkbox_'+i)).is(":checked") )
+				($('#gen_result_'+i)).show()
+			else
+				($('#gen_result_'+i)).hide()
+		}		
+	});
+	//End of generation checkboxes
+
+	//Start of type dropdowns
+	$('#type_1').change(function() {
+		if( ('#type_1').val == '')
+			alert("empty");
+		else
+			alert(('#type_1').val);
+	});
 	
+
+	//end of type dropdowns
 
 
 </script>
-
+<!-- END OF JAVASCRIPT -->
 
 
  <!--- CRITERIOS A CONSIDERAR:
  Experiencia base
- tipos
  Abilidad
  Tamaño de nombre
  tiene forma si/no
