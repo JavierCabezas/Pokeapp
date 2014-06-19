c26f85bbe2cfd3d7a5f149e70c2cd83d5a209440
Fernanda <fernanda.kauak@gmail.com>
2014-06-19 01:35:02 -0400

    Little details on "Players" page.

--- protected/modules/jugadores/views/jugadores/index.php
+++ protected/modules/jugadores/views/jugadores/index.php
@@ -1,12 +1,8 @@
-<h2> Módulo de búsqueda de jugadores </h2>
+<h1 class="jugadores">Jugadores</h1>
 
-<p> 
-	En este módulo de la pokéapp pueden buscar a otros jugadores del juego con intereses similares, buscar algún jugador con un pokémon en específico del
-	Friend Safari (Safari amigo) o simplemente conocer gente nueva. 
-</p>
+<p>En esta sección pueden buscar a otros jugadores del juego con intereses similares, con un pokémon en específico del Friend Safari (Safari Amigo) o simplemente conocer gente nueva.</p>
 
-<p> 
-	Los parámetros actuales que se están guardando en el módulo hasta la fecha son: 
+<p>Los parámetros actuales que se están guardando en el módulo hasta la fecha son: 
 	<ul>
 		<li> Pokémons del safari.</li>
 		<li> TSV. </li>
@@ -14,8 +10,7 @@
 	</ul>
 </p>
 
-<p>
-	Para usar la aplicación pueden:
+<p>Para usar la aplicación pueden:
 	<ul>
 		<li> Crear su perfil en <?php echo CHtml::link('la sección de creación de perfil', array('jugadores/create')) ?> </li>
 		<li> Buscar a otros jugadores en <?php echo CHtml::link('la sección de búsqueda', array('jugadores/buscador')) ?> </li>
