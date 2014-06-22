4673938db391c580433715b2ed5fda73d62e1d4b
Fernanda <fernanda.kauak@gmail.com>
2014-06-22 01:36:09 -0400

    Working on profiles on the players.

--- protected/modules/jugadores/views/jugadores/_view.php
+++ protected/modules/jugadores/views/jugadores/_view.php
@@ -1,89 +1,94 @@
 <div class="view">
 
-	<div class='basic_info'>
-		<div class='nickname'>
-			Nickname: <?php echo $player_nickname ?>
-		</div>
-
-		<div class ='avatar'>
+	<div class="basic_info">
+		<div class="avatar">
 			<?php echo $player_pic ?>
 		</div>
 
-		<div class='friend_code'>
-			Código amigo: <b> <?php echo $friend_code ?> </b>
-		</div>
+		<div class="data">
+			<div class="nickname">
+				<?php echo $player_nickname ?>
+			</div>
 
-		<div class='tsv'>
-			TSV: <?php echo $tsv ?>
+			<div class="friend_code">
+				Friend Code: <b> <?php echo $friend_code ?> </b>
+			</div>
+
+			<div class="tsv">
+				TSV: <?php echo $tsv ?>
+			</div>
+
+			<div class="comments">
+				<?php echo $comment ?>
+			</div>
 		</div>
 	</div> <!-- end of basic info -->
 
-	<div class='safari'>
-		<div class='type'>
+	<div class="safari">
+		<div class="type">
 			Safari: <?php echo $safari_type ?>
 		</div>
 
-		<?php if($safari_type != '(No asignado)'): //Just show the safari pokémon if the safari is defined ?>
-			<div class='safari_pokemon' id='1'>
-				<?php echo $pokemon_safari_1 ?>
-				<?php echo $pic_pokemon_1 ?>
-			</div>
+		<div class="pokes">
+			<?php if($safari_type != '(No asignado)'): //Just show the safari pokémon if the safari is defined ?>
+				<div class="safari_pokemon" id="1">
+					<?php echo $pokemon_safari_1 ?>
+					<?php echo $pic_pokemon_1 ?>
+				</div>
 
-			<div class='safari_pokemon' id='2'>
-				<?php echo $pokemon_safari_2 ?>
-				<?php echo $pic_pokemon_2 ?>
-			</div>
-			
-			<div class='safari_pokemon' id='3'>
-				<?php echo $pokemon_safari_3 ?>
-				<?php echo $pic_pokemon_3 ?>
-			</div>
-			
-		<?php endif; ?>
+				<div class="safari_pokemon" id="2">
+					<?php echo $pokemon_safari_2 ?>
+					<?php echo $pic_pokemon_2 ?>
+				</div>
+				
+				<div class="safari_pokemon" id="3">
+					<?php echo $pokemon_safari_3 ?>
+					<?php echo $pic_pokemon_3 ?>
+				</div>
+				
+			<?php endif; ?>
+		</div>
 	</div> <!-- end of safari -->
 
-	<div class='duel'>
-		<div class='single_<?php echo $duel_single?>'>
+	<div class="duel">
+		<div class="single_<?php echo $duel_single?>">
 			<?php if(!is_null($tier_single)): ?>
 				<?php echo $tier_single ?>
 			<?php endif; ?>
 		</div>
-		<div class='doble_<?php echo $duel_doble?>'>
+		<div class="doble_<?php echo $duel_doble?>">
 			<?php if(!is_null($tier_doble)): ?>
 				<?php echo $tier_doble ?>
 			<?php endif; ?>
 		</div>
-		<div class='triple_<?php echo $duel_triple?>'>
+		<div class="triple_<?php echo $duel_triple?>">
 			<?php if(!is_null($tier_triple)): ?>
 				<?php echo $tier_triple ?>
 			<?php endif; ?>
 		</div>
-		<div class='rotation_<?php echo $duel_triple?>'>
+		<div class="rotation_<?php echo $duel_triple?>">
 			<?php if(!is_null($tier_rotation)): ?>
 				<?php echo $tier_rotation ?>
 			<?php endif; ?>
 		</div>
 	</div> <!-- end of duels  -->
 
-	<div class='contact'>
-		<div class='skype'>
+	<div class="contact">
+		<div class="skype">
 			<?php echo $skype ?>
 		</div>
 	
-		<div class='whatsapp'>
+		<div class="whatsapp">
 			<?php echo $whatsapp ?>
 		</div>
 
-		<div class='facebook'>
+		<div class="facebook">
 			<?php echo $facebook ?>
 		</div>
 
-		<div class='others'>
+		<div class="others">
 			<?php echo $others ?>
 		</div> <!-- end of contact info -->
 	</div>
-
-	<div class='comments'>
-		<?php echo $comment ?>
-	</div>
+	
 </div>
--- protected/modules/jugadores/views/jugadores/index.php
+++ protected/modules/jugadores/views/jugadores/index.php
@@ -2,10 +2,10 @@
 
 <nav>
 	<ul>
-		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_01.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/jugadores/create'))?> </li>
-		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_02.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/jugadores/buscador'))?> </li>
-		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_03.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/jugadores/nuevoCodigo'))?> </li>
-		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_04.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/jugadores/updateForm'))?> </li>
+		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_01.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/create'))?> </li>
+		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_02.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/buscador'))?> </li>
+		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_03.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/nuevoCodigo'))?> </li>
+		<li><?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/pj_nav/n_pj_04.png" width="210" height="100" />',  Yii::app()->createurl('jugadores/updateForm'))?> </li>
 	</ul>
 </nav>
 
@@ -28,14 +28,3 @@
 		<p>(separados por tipo de duelo y tier).</p>
 	</div>
 </div>
-
-
-
-
-
-
-	<!--<li> Creación de perfil: <?php echo CHtml::link('Crear nuevo', array('jugadores/create')) ?> </li>
-		<li> Búsqueda de jugadores: en <?php echo CHtml::link('Buscar jugadores', array('jugadores/buscador')) ?> </li>
-		<li> Obtención de código de modificación de perfil: <?php echo CHtml::link('Obtener nuevo código', array('jugadores/nuevoCodigo')) ?> </li> 
-		<li> Modificar mi perfil: <?php echo CHtml::link('Modificar', array('jugadores/updateForm')) ?> </li>
-	</ul>-->
