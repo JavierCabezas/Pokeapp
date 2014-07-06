463ff95df31423f94d5e322d9c219f21c419f71b
Fernanda <fernanda.kauak@gmail.com>
2014-07-05 23:12:48 -0400

    New order of info display (players index).

--- protected/modules/jugadores/views/jugadores/index.php
+++ protected/modules/jugadores/views/jugadores/index.php
@@ -14,19 +14,25 @@
 	<p>Los parámetros actuales que se están guardando en el módulo hasta la fecha son:</p>
 	<div class="param">
 		<?php echo CHtml::link("<img src='".imageDir()."/jugadores_info/jug_001.png' />", array('')) ?>
-		<?php echo CHtml::link('<h4>Pokémon Safari</h4>', array(''))?>
-		<p>Tipo y especie de Pokémon que da el usuario por medio de su Friend Code.</p>
+		<div class="info">
+			<?php echo CHtml::link('<h4>Pokémon Safari</h4>', array(''))?>
+			<p>Tipo y especie de Pokémon que da el usuario por medio de su Friend Code.</p>
+		</div>
 	</div>
 	<div class="param">
 		<?php echo CHtml::link("<img src='".imageDir()."/jugadores_info/jug_002.png' />", array('')) ?>
-		<?php echo CHtml::link('<h4>Trainer Shiny Value</h4>', array('')) ?>
-		<p> Busca jugadores que te puedan asegurar huevos shiny por medio del TSV (Trainer shiny value).</p>
+		<div class="info">
+			<?php echo CHtml::link('<h4>Trainer Shiny Value</h4>', array('')) ?>
+			<p>Busca jugadores que te puedan asegurar huevos shiny por medio del TSV (Trainer shiny value).</p>
+		</div>
 	</div>
 	<div class="param">
 		<?php echo CHtml::link("<img src='".imageDir()."/jugadores_info/jug_003.png' />", array('')) ?>
-		<?php echo CHtml::link('<h4>Interés en duelos</h4>', array(''))?>
-		<p>Puedes buscar jugadores no solo por tipo de duelo (single, doble, triple o rotación) sino que también por Tiers.</p>
+		<div class="info">
+			<?php echo CHtml::link('<h4>Interés en duelos</h4>', array(''))?>
+			<p>Busca jugadores no sólo por preferencia de tipo de duelo (single, doble, triple o rotación) sino que también por Tiers.</p>
+		</div>
 	</div>
 
-	<p> Para ver más información acerca de cualquiera de estas temáticas haz click en la imagen destacada de esta. </p>
+	<p style="clear:both"> Para ver más información acerca de cualquiera de estas temáticas haz click en la imagen destacada de esta. </p>
 </div>
