6493809abe98a0c8e73431ba4d7a1fa8c4365382
Javier Cabezas <jcleyton@gmail.com>
2014-07-09 16:05:54 -0400

    Fix image mail

--- protected/views/mail/mailView.php
+++ protected/views/mail/mailView.php
@@ -1,6 +1,6 @@
 <div id='content' style="font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif; color: #000; text-decoration: none;height: 300px; width: 100%; margin-right: auto; margin-left: auto;">
   <div id='header' style="height: auto; width: 100%; margin-right: auto; margin-left: auto; padding-bottom: 8.1%; border-bottom: #c54247 2px solid;">
-    <div id='logo' style="float: left; height: 65px; width: 200px; margin-left: 10px; margin-top: auto; margin-bottom: auto;"><img src='<?php echo imageDir()?>/logo_mail.png' width='200' height='65' /></div>
+    <div id='logo' style="float: left; height: 65px; width: 200px; margin-left: 10px; margin-top: auto; margin-bottom: auto;"><img src='http://pokedaisuki.cl/pokeapp/images/logo_mail.png' width='200' height='65' /></div>
   </div>
   <div class='info' style="font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif; font-size: 14px; color: #000000; text-decoration: none; padding: 10px; margin-top: 10px; margin-bottom: 10px;">
 	<div class="title" style="line-height: 100%; width: 100%; height: 100%; font-size: 24px; font-weight: bold; color: #c54247; float: left; margin-top: 0px; margin-bottom: 15px;"><?php echo $mail_title?></div>
