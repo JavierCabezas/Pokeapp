cbdf161adad9b7e0d21b9f45b8a7d5e78390ab7a
Fernanda <fernanda.kauak@gmail.com>
2014-08-06 00:35:47 -0400

    New titles and GUI on tournament section.

--- protected/modules/tournament/views/tournamentPokemon/create.php
+++ protected/modules/tournament/views/tournamentPokemon/create.php
@@ -8,9 +8,11 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1>Agregar nuevo pokémon</h1>
+<h1 class="nuevopkmn">Agregar nuevo pokémon</h1>
 
-<?php echo $this->renderPartial('_form', array(
-	'model'				=> $model,
-    'array_tournament'	=> $array_tournament,
-)); ?>
+<div class="infosec">
+	<?php echo $this->renderPartial('_form', array(
+		'model'				=> $model,
+	    'array_tournament'	=> $array_tournament,
+	)); ?>
+</div>
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -12,41 +12,43 @@
 		    }
 		?>
 
-		<h1 class="sectiontitle">Iniciar sesión</h1>
+		<h1 class="login">Iniciar sesión</h1>
 
-		<p> En caso de que hayas olvidado tu código (contraseña) de login puedes pedir otro <?php echo CHtml::link('en el siguiente link', array('/usuario/resetearCodigo')) ?>
-		<div class="login-form">
-			<div class="form">
-			<?php $form=$this->beginWidget('CActiveForm', array(
-				'id'=>'login-form',
-				'enableClientValidation'=>true,
-				'clientOptions'=>array(
-					'validateOnSubmit'=>true,
-				),
-			)); ?>
+		<div class="infosec">
+			<p> En caso de que hayas olvidado tu código (contraseña) de login puedes pedir otro <?php echo CHtml::link('en el siguiente link', array('/usuario/resetearCodigo')) ?>
+			<div class="login-form">
+				<div class="form">
+				<?php $form=$this->beginWidget('CActiveForm', array(
+					'id'=>'login-form',
+					'enableClientValidation'=>true,
+					'clientOptions'=>array(
+						'validateOnSubmit'=>true,
+					),
+				)); ?>
 
-				<p class="note">Campos con <span class="required">*</span> son requeridos.</p>
-				<div class="row">
-					<?php echo $form->labelEx($model,'username'); ?>
-					<?php echo $form->textField($model,'username'); ?>
-					<?php echo $form->error($model,'username'); ?>
-				</div>
-				<div class="row">
-					<?php echo $form->labelEx($model,'password'); ?>
-					<?php echo $form->passwordField($model,'password'); ?>
-					<?php echo $form->error($model,'password'); ?>
-				</div>
-				<div class="row rememberMe">
-					<?php echo $form->checkBox($model,'rememberMe'); ?>
-					<?php echo $form->label($model,'rememberMe'); ?>
-					<?php echo $form->error($model,'rememberMe'); ?>
-				</div>
-				<div class="row buttons">
-					<?php echo CHtml::submitButton('Login'); ?>
-				</div>
+					<p class="note">Campos con <span class="required">*</span> son requeridos.</p>
+					<div class="row">
+						<?php echo $form->labelEx($model,'username'); ?>
+						<?php echo $form->textField($model,'username'); ?>
+						<?php echo $form->error($model,'username'); ?>
+					</div>
+					<div class="row">
+						<?php echo $form->labelEx($model,'password'); ?>
+						<?php echo $form->passwordField($model,'password'); ?>
+						<?php echo $form->error($model,'password'); ?>
+					</div>
+					<div class="row rememberMe">
+						<?php echo $form->checkBox($model,'rememberMe'); ?>
+						<?php echo $form->label($model,'rememberMe'); ?>
+						<?php echo $form->error($model,'rememberMe'); ?>
+					</div>
+					<div class="row buttons">
+						<?php echo CHtml::submitButton('Login'); ?>
+					</div>
 
-				<?php $this->endWidget(); ?>
+					<?php $this->endWidget(); ?>
 				</div><!-- form -->
 			</div>
 		</div>
+	</div>
 </div>
