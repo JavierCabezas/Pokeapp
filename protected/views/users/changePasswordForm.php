8e062318311f7c89951783c6e53d6228001a5b68
Javier Cabezas <jcleyton@gmail.com>
2014-10-19 12:05:33 -0300

    Added wells

--- protected/views/users/changePasswordForm.php
+++ protected/views/users/changePasswordForm.php
@@ -12,34 +12,35 @@ $this->breadcrumbs=array(
     }
 ?>
 
-<h1> Cambio de contraseña </h1>
+<div class='well'>
+	<h1> Cambio de contraseña </h1>
 
-<p>
-	Puedes usar el siguiente formulario en caso de que prefieras tener una contraseña creada por ti en vez de la generada automáticamente por el sistema.
-</p>
+	<p>
+		Puedes usar el siguiente formulario en caso de que prefieras tener una contraseña creada por ti en vez de la generada automáticamente por el sistema.
+	</p>
 
-<p>
-	Te pedimos tu contraseña anterior para evitar que, en caso de que dejes tu sesión abierta en algún equipo, alguna persona te cambie la contraseña de forma malintencionada. Si no la recuerdas puedes resetearla en <?php echo CHtml::link('el siguiente link', array('/Usuarios/resetearCodigo')) ?>.
-</p>
+	<p>
+		Te pedimos tu contraseña anterior para evitar que, en caso de que dejes tu sesión abierta en algún equipo, alguna persona te cambie la contraseña de forma malintencionada. Si no la recuerdas puedes resetearla en <?php echo CHtml::link('el siguiente link', array('/Usuarios/resetearCodigo')) ?>.
+	</p>
 
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-	'id'=>'user-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+		'id'=>'user-form',
+		'enableAjaxValidation'=>false,
+	)); ?>
 
-	<?php echo $form->passwordFieldRow($model,'oldpassword',array('class'=>'span5','maxlength'=>100)); ?>
+		<?php echo $form->passwordFieldRow($model,'oldpassword',array('class'=>'span5','maxlength'=>100)); ?>
 
-	<?php echo $form->passwordFieldRow($model,'password',array('class'=>'span5','maxlength'=>100)); ?>
+		<?php echo $form->passwordFieldRow($model,'password',array('class'=>'span5','maxlength'=>100)); ?>
 
-	<?php echo $form->passwordFieldRow($model,'repeatpassword',array('class'=>'span5','maxlength'=>100)); ?>
-	
-<div class="form-actions">
-	<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>'Resetear contraseña',
-		)); ?>
+		<?php echo $form->passwordFieldRow($model,'repeatpassword',array('class'=>'span5','maxlength'=>100)); ?>
+		
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+				'buttonType'=>'submit',
+				'type'=>'primary',
+				'label'=>'Resetear contraseña',
+			)); ?>
+	</div>
+
+	<?php $this->endWidget(); ?>
 </div>
-
-<?php $this->endWidget(); ?>
-
--- protected/views/users/resetCodeForm.php
+++ protected/views/users/resetCodeForm.php
@@ -12,28 +12,29 @@ $this->breadcrumbs=array(
         echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
     }
 ?>
+<div class='well'>
+	<h1> Reseteo de contraseña </h1>
 
-<h1> Reseteo de contraseña </h1>
-
-<p>
-	Para poder recibir una nueva contraseña ingresa tu correo electrónico en el formulario de  más abajo y, en caso de que esté registrado en
-	nuestra base de datos, se te enviará un correo con las instrucciones para realizar el proceso.
-</p>
+	<p>
+		Para poder recibir una nueva contraseña ingresa tu correo electrónico en el formulario de  más abajo y, en caso de que esté registrado en
+		nuestra base de datos, se te enviará un correo con las instrucciones para realizar el proceso.
+	</p>
 
 
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-	'id'=>'user-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+		'id'=>'user-form',
+		'enableAjaxValidation'=>false,
+	)); ?>
 
-	<?php echo $form->textFieldRow($model,'mail',array('class'=>'span5','maxlength'=>100)); ?>
-	
-<div class="form-actions">
-	<?php $this->widget('bootstrap.widgets.TbButton', array(
-			'buttonType'=>'submit',
-			'type'=>'primary',
-			'label'=>'Enviar correo',
-		)); ?>
+		<?php echo $form->textFieldRow($model,'mail',array('class'=>'span5','maxlength'=>100)); ?>
+		
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+				'buttonType'=>'submit',
+				'type'=>'primary',
+				'label'=>'Enviar correo',
+			)); ?>
+	</div>
+
+	<?php $this->endWidget(); ?>
 </div>
-
-<?php $this->endWidget(); ?>
