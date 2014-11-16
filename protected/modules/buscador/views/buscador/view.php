8884836a27a0e71c828c4b5cb1da9c6d6b41d7cb
Fernanda <fernanda.kauak@gmail.com>
2014-11-15 23:01:29 -0300

    Fixing "pokémon detail" view.

--- protected/modules/buscador/views/buscador/view.php
+++ protected/modules/buscador/views/buscador/view.php
@@ -2,44 +2,57 @@
 	/** Renders the view to see the information about a certain pokémon. **/
 ?>
 
-<h2> Información de <?php echo $pokemon->pokemonName ?> </h2>
+<div id="pkmn_title">
+	<div class="avatar">
+		<?php echo $pokemon->species->image('moving') ?> 
+	</div>
+	<div class="info">
+		#<?php echo $pokemon->species->id ?> (Generación #<?php echo $pokemon->generation ?>).
+		<h2> <?php echo $pokemon->pokemonName ?> </h2>
+		<div class="info1">
+			<h4> Tamaño: </h4>
+			<?php echo $pokemon->height/10 ?> [m]
+		</div>
+		<div class="info2">
+			<h4> Peso: </h4>
+			<?php echo $pokemon->weight/10 ?> [kg]
+		</div>
+		<div class="info3">
+			<h4> Tipo(s): </h4>
+			<?php foreach($types as $type): ?>
+				<?php echo $type->type->image() ?>
+			<?php endforeach ?>
+		</div>
+	</div>
+	<div class="other">
+		<div class="info1">
+			<h4> Color: </h4>
+			<?php echo $pokemon->species->color->colorName ?> 
+		</div>
+		<div class="info2">
+			<h4> Forma: </h4>
+			<?php echo $pokemon->species->shape->shapeName ?> 
+		</div>
+		<div class="info3">
+			<h4> Grupo(s) huevo </h4>
+			<ul>
+				<?php foreach($eggies as $eggie): ?>
+				<li> <?php echo $eggie->eggGroup->eggGroupName ?> </li>
+				<?php endforeach ?>
+			</ul>
+		</div>
+	</div>
+	<div class="hab">
+		<h3> Habilidades </h3>
+		<ul>
+		<?php foreach($abilities as $ability): ?>
+			<li> <?php echo  $ability->ability->abilityName ?> </li>
+		<?php endforeach ?>
+		</ul>
+	</div>
+</div>
 
-<?php echo $pokemon->species->image('moving') ?> 
 
-<h3> Número </h3>
-#<?php echo $pokemon->species->id ?> (Generación #<?php echo $pokemon->generation ?>).
-
-<h3> Tamaño: </h3>
-<?php echo $pokemon->height/10 ?> [m]
-
-<h3> Peso: </h3>
-<?php echo $pokemon->weight/10 ?> [kg]
-
-<h3> Tipo(s): </h3>
-<?php foreach($types as $type): ?>
-	<?php echo $type->type->image() ?>
-<?php endforeach ?>
-
-<h3> Color: </h3>
-<?php echo $pokemon->species->color->colorName ?> 
-
-<h3> Forma: </h3>
-<?php echo $pokemon->species->shape->shapeName ?> 
-
-
-<h3> Grupo(s) huevo </h3>
-<ul>
-<?php foreach($eggies as $eggie): ?>
-	<li> <?php echo $eggie->eggGroup->eggGroupName ?> </li>
-<?php endforeach ?>
-</ul>
-
-<h3> Habilidades </h3>
-<ul>
-<?php foreach($abilities as $ability): ?>
-	<li> <?php echo  $ability->ability->abilityName ?> </li>
-<?php endforeach ?>
-</ul>
 
 <h3> Resistencias e inmunidades: </h3>
 <table class='typeTable'>
